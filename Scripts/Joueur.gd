b9cbec7983194886dad36b09a88ebeb91c0526e8
RedYou <63940364+RedYou1@users.noreply.github.com>
2021-11-02 19:53:13 -0400

    tire et physics

--- Scripts/Joueur.gd
+++ Scripts/Joueur.gd
@@ -1,9 +1,19 @@
-extends "res://Scripts/Personnage.GD"
+extends "res://Scripts/Personnage.gd"
 
 func _process(delta):
+	
 	if Input.is_action_pressed("DROIT"):
 		move_and_collide(Vector2(self.vitesse,0))
+		self.animation.flip_h = false
 	if Input.is_action_pressed("GAUCHE"):
 		move_and_collide(Vector2(-self.vitesse,0))
+		self.animation.flip_h = true
+	
 	if Input.is_action_just_pressed("HAUT") and self.is_on_floor:
 		self.velocityY = -15
+	
+	if Input.is_action_just_pressed("TIRER"):
+		self.tir = true
+		self.tirer()
+	if Input.is_action_just_released("TIRER"):
+		self.tir = false
