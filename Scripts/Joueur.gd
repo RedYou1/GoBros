7f41bec995474bf185bd019bf4595900cb63c06e
RedYou <63940364+RedYou1@users.noreply.github.com>
2021-11-02 18:13:05 -0400

    gravity personnage

--- Scripts/Joueur.gd
+++ Scripts/Joueur.gd
@@ -1,7 +1,9 @@
-extends "res://Scripts/personnage.GD"
+extends "res://Scripts/Personnage.GD"
 
 func _process(delta):
-	if (Input.is_action_pressed("DROIT")):
+	if Input.is_action_pressed("DROIT"):
 		move_and_collide(Vector2(self.vitesse,0))
-	if (Input.is_action_pressed("GAUCHE")):
+	if Input.is_action_pressed("GAUCHE"):
 		move_and_collide(Vector2(-self.vitesse,0))
+	if Input.is_action_just_pressed("HAUT") and self.is_on_floor:
+		self.velocityY = -15
--- Scripts/Personnage.gd
+++ Scripts/Personnage.gd
@@ -1,10 +1,16 @@
 extends KinematicBody2D
 
 export(float) var vitesse = 10
-export(float) var GRAVITY = 0.098
+export(float) var GRAVITY = 0.98
 
 var velocityY = 0
+var is_on_floor = false
 
 func _physics_process(delta):
 	velocityY += GRAVITY
-	move_and_collide(Vector2(0,velocityY))
+	var t = move_and_collide(Vector2(0,velocityY))
+	if t != null:
+		is_on_floor = true
+		velocityY = 0
+	else:
+		is_on_floor = false
