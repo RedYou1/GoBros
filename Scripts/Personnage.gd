d8856eda50f292cec8fce2a167e6706eea87c770
RedYou <63940364+RedYou1@users.noreply.github.com>
2021-11-02 16:18:54 -0400

    p

--- Scripts/Personnage.gd
+++ Scripts/Personnage.gd
@@ -1,3 +1,10 @@
 extends KinematicBody2D
 
 export(float) var vitesse = 10
+export(float) var GRAVITY = 0.098
+
+var velocityY = 0
+
+func _physics_process(delta):
+	velocityY += GRAVITY
+	move_and_collide(Vector2(0,velocityY))
