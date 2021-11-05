278ceb697631a5665d127aaeb4d764cb2e352e49
RedYou <63940364+RedYou1@users.noreply.github.com>
2021-11-04 21:14:05 -0400

    meilleur collision entre balle et boite

--- Scripts/Balle.gd
+++ Scripts/Balle.gd
@@ -1,12 +1,21 @@
-extends KinematicBody2D
+extends Node2D
 
-export(float) var direction = 10
+export(float) var direction = 200
 export(int) var damage = 1
 
+var ray
+
+func _ready():
+	ray = get_node("RayCast2D")
+
 func _physics_process(delta):
-	var col = move_and_collide(Vector2(direction,0))
+	ray.cast_to.x = direction * delta
 	
-	if col != null:
-		if col.collider.has_method("hit"):
-			col.collider.hit(self,damage)
+	var col = ray.get_collider()
+	
+	if col == null:
+		position.x += direction * delta
+	else:
+		if col.has_method("hit"):
+			col.hit(self,damage)
 		queue_free()
