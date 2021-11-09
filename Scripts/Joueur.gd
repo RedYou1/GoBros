6d59b7212794eacb6a57628151b5f74fdc2b7c28
RedYou <jcdemers123@gmail.com>
2021-11-09 16:11:07 -0500

    debut remettre les block

--- Scripts/Joueur.gd
+++ Scripts/Joueur.gd
@@ -1,12 +1,14 @@
 extends "res://Scripts/Personnage.gd"
 
+export(float) var vitesse = 2
+
 func _process(delta):
 	
 	if Input.is_action_pressed("DROIT"):
-		move_and_collide(Vector2(self.vitesse,0))
+		move_and_collide(Vector2(vitesse,0))
 		self.animation.flip_h = false
 	if Input.is_action_pressed("GAUCHE"):
-		move_and_collide(Vector2(-self.vitesse,0))
+		move_and_collide(Vector2(-vitesse,0))
 		self.animation.flip_h = true
 	
 	if Input.is_action_just_pressed("HAUT") and self.is_on_floor:
