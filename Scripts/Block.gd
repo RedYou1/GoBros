f511e8a60cc67dba3359e4dc25aa12927c5d2e78
RedYou <jcdemers123@gmail.com>
2021-11-09 23:19:26 -0500

    explosion block

--- Scripts/Block.gd
+++ Scripts/Block.gd
@@ -3,9 +3,21 @@ extends RigidBody2D
 
 export(int) var vie = 5
 
+var explosion
+
+func _ready():
+	explosion = get_node("explosion")
 
 func hit(from,damage):
+	print(vie)
 	vie -= damage
 	apply_impulse(from.position-position,Vector2(from.direction,0))
 	if vie <= 0:
-		queue_free()
+		get_node("Sprite").visible = false
+		get_node("CollisionShape2D").disabled = true
+		explosion.visible = true
+		explosion.playing = true
+
+
+func _on_explosion_animation_finished():
+	queue_free()
