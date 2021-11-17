99b1cc0e92b30570fd274461062041029be8a91f
lol <jchiasson423@hotmail.com>
2021-11-16 22:01:25 -0500

    Ajout des backgrounds

--- Scripts/Background.gd
+++ Scripts/Background.gd
@@ -7,7 +7,7 @@ extends ParallaxBackground
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	get_node("ParallaxLayer").motion_mirroring.x = get_node("ParallaxLayer/Back1").rect_size.x * get_node("ParallaxLayer/Back1").rect_scale.x
-	get_node("ParallaxLayer").motion_mirroring.y = (get_node("ParallaxLayer/Back1").rect_size.y * get_node("ParallaxLayer/Back1").rect_scale.y) * 4
+	get_node("ParallaxLayer").motion_mirroring.y = get_node("ParallaxLayer/Back1").rect_size.y * get_node("ParallaxLayer/Back1").rect_scale.y
 
 
 func _process(delta: float) -> void:
---
+++ Scripts/Background1.gd
@@ -0,0 +1,17 @@
+extends "res://Scripts/Background.gd"
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	get_node("ParallaxLayer").motion_mirroring.x = get_node("ParallaxLayer/Back1").rect_size.x * get_node("ParallaxLayer/Back1").rect_scale.x
+	get_node("ParallaxLayer").motion_mirroring.y = (get_node("ParallaxLayer/Back1").rect_size.y * get_node("ParallaxLayer/Back1").rect_scale.y)*4
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
