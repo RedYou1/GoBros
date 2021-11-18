3b6f0e174865db6467a4f4a59271f23905d0aa7f
lol <jchiasson423@hotmail.com>
2021-11-18 12:14:12 -0500

    Changement de l'écran de loading et du logo

--- Scripts/BossLevel.gd
+++ Scripts/BossLevel.gd
@@ -3,6 +3,7 @@ extends "res://Scripts/Niveau.gd"
 
 # Declare member variables here. Examples:
 var mem_position_apical
+var mem_hauteur = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,6 +14,7 @@ func _physics_process(delta):
 	if get_node("Apical"):
 		mem_position_apical = get_node("Apical").position
 	else:
-		if get_node("Joueur").is_on_floor:
+		if get_node("Joueur").is_on_floor && !mem_hauteur:
 			get_node("FinNiveau").position = Vector2(mem_position_apical.x, get_node("Joueur").position.y)
+			mem_hauteur = true
 			
